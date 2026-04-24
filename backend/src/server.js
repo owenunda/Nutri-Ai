4120ef2a0f7f91d534a24e6b80543e1c0579f33c
owenunda <oweenunda.dev@gmail.com>
2026-04-24 00:07:44 -0500

    feat: creda la coneccion base a la base de datos

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,15 @@
 import 'dotenv/config';
 import app from './app.js';
 import { config } from './config/env_config.js';
+import pool from './database/conection.js';
 
 const PORT = config.port;
+
+pool.connect()
+  .then(() => console.info('Base de datos conectada'))
+  .catch(err => {
+    throw new AppError('Error al conectar a la base de datos', 500, 'DATABASE_ERROR', err)
+  })
 
 app.listen(PORT, () => {
   console.log(`El servidor está corriendo en el puerto http://localhost:${PORT}`);
