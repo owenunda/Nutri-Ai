3528e99dc9d6f92b41ae348734ff7c3f12ebd1a6
SamuelSalazar1608 <samuelsalazar1608@gmail.com>
2026-05-06 20:02:33 -0500

    feat/User-physical-data-logger

--- backend/src/modules/user/user.routes.js
+++ backend/src/modules/user/user.routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import authenticateToken from '../../middleware/auth.middleware.js';
-import { getProfile } from './user.controller.js';
+import { createPhysical, getProfile } from './user.controller.js';
+import { validateCreatePhysicalRecordRequest } from './user.validation.js';
 
 const router = Router();
 
 router.get('/profile', authenticateToken(), getProfile);
+router.post('/physical', authenticateToken(), validateCreatePhysicalRecordRequest, createPhysical);
 
 export default router;
