4ecfd2dce3b7cc79e87f62d704a23aac75923495
owenunda <oweenunda.dev@gmail.com>
2026-06-16 01:04:28 -0500

    refactor: simplify API base URL to a static production endpoint

--- nutriapp/lib/core/constants/api_base_url.dart
+++ nutriapp/lib/core/constants/api_base_url.dart
@@ -1,21 +1,5 @@
-import 'package:flutter/foundation.dart';
-
 class ApiBaseUrl {
   static String get value {
-    if (kReleaseMode) {
-      // Reemplaza esta URL con tu servidor de producción real
-      return 'https://api.tu-servidor-produccion.com/api/v1';
-    }
-
-    if (kIsWeb) {
-      return 'http://localhost:3000/api/v1';
-    }
-
-    switch (defaultTargetPlatform) {
-      case TargetPlatform.android:
-        return 'http://10.0.2.2:3000/api/v1';
-      default:
-        return 'http://localhost:3000/api/v1';
-    }
+    return 'https://api.nutri.oween.software/api/v1';
   }
 }
