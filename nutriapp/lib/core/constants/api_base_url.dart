c72461709400ec8e62b33db8c87d2b10b1f3daf0
Julipv <vasquezjuli310@gmail.com>
2026-06-22 14:00:17 -0500

    simplify api url

--- nutriapp/lib/core/constants/api_base_url.dart
+++ nutriapp/lib/core/constants/api_base_url.dart
@@ -1,12 +1,5 @@
-import 'dart:io';
-import 'package:flutter/foundation.dart';
-
 class ApiBaseUrl {
   static String get value {
-    if (kDebugMode) {
-      return Platform.isAndroid ? 'http://10.0.2.2:3000/api/v1' : 'http://localhost:3000/api/v1';
-    }
     return 'https://api.nutri.oween.software/api/v1';
   }
 }
-
